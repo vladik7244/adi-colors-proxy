3302acfc5f4cada4391f2015858487b3c41f9c12
Vlad Harahan <vladyslavh@wix.com>
2017-10-03 14:49:04 +0300

    added proxy to colors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const request = require('request');
+const cors = require('cors');
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
+app.use(cors());
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -11,6 +14,17 @@ const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   res.render('index.html', {pageCountMessage: null});
+});
+
+app.get('/colors', function (req, res, next) {
+  request(`http://www.wix.com/_api/onboarding-custom-pallete-server-webapp/coloring/colorMatches?color=${req.query.color}&limit=${req.query.limit || 6}`, (err, response, body) => {
+    if (err) {
+      res.send({
+        error: true,
+      })
+    }
+    res.send(body);
+  });
 });
 
 // error handling
